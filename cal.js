8152ff37b0bfb22542d3f190d7eb5df739b70c14
Leena3069 <133123456+Leena3069@users.noreply.github.com>
2023-05-19 17:50:39 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,13 @@
+const result = document.getElementById("result");
+
+function addToResult(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+}
+
+function calculateResult() {
+  result.value = eval(result.value);
+}
